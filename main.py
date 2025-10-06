1772166b8c4ca50463850bc95a8c7be3e9bdac8a
masyaenot-blip <masya.enot@gmail.com>
2025-10-06 17:24:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def main():
     logger.info("Starting polling...")
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except Exception:
